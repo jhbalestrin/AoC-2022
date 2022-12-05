33e48b428d212bd8964eb86b0623b85c4d01f4cb
jhbalestrin <jhbalestrin@gmail.com>
2022-12-04 23:47:00 -0300

    Finished streaming solution for js language on problem 1

--- javascript/01/streaming/elfCaloriesProcessor.js
+++ javascript/01/streaming/elfCaloriesProcessor.js
@@ -3,25 +3,24 @@ import { Transform } from 'stream'
 export class elfCaloriesProcessor extends Transform{
 	constructor() {
 		super({
-			writableObjectMode: true
+			writableObjectMode: true,
+			readableObjectMode: true
 		})
 		this.calories = 0
 	}
 	_transform(obj, encoding, callback) {
-		if(obj === "end"){
-			return callback(null, `end`)
+		if(typeof obj === "symbol"){
+			this.push(obj)
 		}
 		if(typeof obj === "number"){
 			this.calories += obj
 			// return callback(null, `Elf received ${obj} has total ${this.calories} calories\n`)
-			return
 		}
 		if(typeof obj === "string"){
 			// callback(null,`Elf finished with ${this.calories} calories\n`)
-			callback(null, this.calories)
+			this.push( this.calories)
 			this.calories = 0
-			return
 		}
-		callback(new Error("Invalid input"))
+		callback()
 	}
 }
--- javascript/shared/stream/inputAdapters/fileAdapter.js
+++ javascript/shared/stream/inputAdapters/fileAdapter.js
@@ -1,8 +1,11 @@
 import { existsSync, createReadStream } from "fs"
 
 export const FileAdapter = (filePath) => {
-	if(!filePath || !existsSync(filePath)){
+	if(!filePath){
 		throw new Error("Missing file path on file adapter")
+	}
+	if(!existsSync(filePath)){
+		throw new Error("Files does not exist")
 	}
 	return {
 		stream: () => createReadStream(filePath)
